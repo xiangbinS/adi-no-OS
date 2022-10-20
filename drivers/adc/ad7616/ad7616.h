c4f4f56bb1aa8a12bd3fe556c74c2181677faa8c
Stanca Pop <stanca.pop@analog.com>
2022-10-20 16:34:34 +0300

    ad7616: Fix conflicts

--- drivers/adc/ad7616/ad7616.h
+++ drivers/adc/ad7616/ad7616.h
@@ -41,6 +41,11 @@
 #define AD7616_H_
 
 #include "no_os_gpio.h"
+#include "no_os_pwm.h"
+#include "app_config.h"
+#if (HDL_AD7616_PARALLEL == 0)
+#include "spi_engine.h"
+#endif
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
@@ -90,6 +95,7 @@
 #define AD7616_STATUS_B(x)				(((x) & 0xF) << 8)
 #define AD7616_STATUS_CRC(x)			(((x) & 0xFF) << 0)
 
+#define AD7616_TRIGGER_PULSE_WIDTH_NS	        50
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
@@ -151,6 +157,8 @@ struct ad7616_dev {
 	struct no_os_gpio_desc	*gpio_os0;
 	struct no_os_gpio_desc	*gpio_os1;
 	struct no_os_gpio_desc	*gpio_os2;
+	/** PWM */
+	struct no_os_pwm_desc *trigger_pwm_desc;
 	/* AXI Core */
 	uint32_t core_baseaddr;
 	/* Device Settings */
@@ -174,6 +182,8 @@ struct ad7616_init_param {
 	struct no_os_gpio_init_param		*gpio_os0_param;
 	struct no_os_gpio_init_param		*gpio_os1_param;
 	struct no_os_gpio_init_param		*gpio_os2_param;
+	/** PWM */
+	struct no_os_pwm_init_param             *trigger_pwm_init;
 	/* Core */
 	uint32_t			core_baseaddr;
 	/* Device Settings */
