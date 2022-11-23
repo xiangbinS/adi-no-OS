c7668424dcf3f21a163871e7ef42beb2513eea5d
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-11-23 10:24:51 +0200

    ci: force cppcheck to version 1.90
    Use specific version for the cppcheck.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/cppcheck.sh
+++ ci/cppcheck.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-sudo apt-get install cppcheck 
+wget --no-check-certificate "https://sourceforge.net/projects/cppcheck/files/cppcheck/1.90/cppcheck-1.90.tar.gz"
+tar -xzf cppcheck-1.90.tar.gz
+cd ./cppcheck-1.90
+cmake .
+make -j${NUM_JOBS} && sudo make install
+cd .. && rm -rf ./cppcheck-1.90
 
 [[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
 	
