6a398fce4bd9494152c0861126edf07fc2142273
Ciprian Regus <ciprian.regus@analog.com>
2023-02-06 13:34:28 +0200

    lwip wip

--- projects/eval-adxl355-pmdz/src/platform/maxim/parameters.h
+++ projects/eval-adxl355-pmdz/src/platform/maxim/parameters.h
@@ -59,7 +59,7 @@
 #define UART_IRQ_ID     UART0_IRQn
 
 #define UART_DEVICE_ID  0
-#define UART_BAUDRATE   57600
+#define UART_BAUDRATE   3686400
 #define UART_EXTRA      &adxl355_uart_extra_ip
 #define UART_OPS        &max_uart_ops
 
