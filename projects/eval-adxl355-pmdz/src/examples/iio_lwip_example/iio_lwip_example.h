a7a148f01115ecd3d30dca8d31b0df5c75e17e2a
Ciprian Regus <ciprian.regus@analog.com>
2023-05-19 11:45:13 +0300

    projects: eval-adxl355pmdz: IIO networking example
    Add an example of using the ADXL355 with the IIOD network backend
    (+adin1110). This uses the ADIN1110EBZ as a carrier.
    
    Explicitly set the HARDWARE variable to the current SDP-K1 ioc file when
    building the previous examples.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- projects/eval-adxl355-pmdz/src/examples/iio_lwip_example/iio_lwip_example.h
+++ projects/eval-adxl355-pmdz/src/examples/iio_lwip_example/iio_lwip_example.h
@@ -1,6 +1,6 @@
 /***************************************************************************//**
- *   @file   eval-adxl355-pmdz/src/common/common_data.h
- *   @brief  Defines common data to be used by eval-adxl355-pmdz examples.
+ *   @file   iio_example.h
+ *   @brief  IIO example header for eval-adxl355-pmdz project
  *   @author RBolboac (ramona.bolboaca@analog.com)
 ********************************************************************************
  * Copyright 2022(c) Analog Devices, Inc.
@@ -36,41 +36,16 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
-#ifndef __COMMON_DATA_H__
-#define __COMMON_DATA_H__
+#ifndef __IIO_LWIP_EXAMPLE_H__
+#define __IIO_LWIP_EXAMPLE_H__
 
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
-#include "platform_includes.h"
-#include "adxl355.h"
-#ifdef IIO_SUPPORT
-#include "iio_adxl355.h"
-#ifdef IIO_TRIGGER_EXAMPLE
-#include "iio_trigger.h"
-#endif
-#ifdef IIO_LWIP_EXAMPLE
-#include "adin1110.h"
-#endif
-#endif
 
 /******************************************************************************/
-/********************** Macros and Constants Definitions **********************/
+/************************ Functions Declarations ******************************/
 /******************************************************************************/
-extern struct no_os_uart_init_param adxl355_uart_ip;
-extern struct no_os_spi_init_param adxl355_spi_ip;
-extern struct adxl355_init_param adxl355_ip;
+int iio_lwip_example_main();
 
-#ifdef IIO_TRIGGER_EXAMPLE
-#define ADXL355_GPIO_TRIG_NAME "adxl355-dev0"
-
-extern struct iio_hw_trig_init_param adxl355_gpio_trig_ip;
-extern struct no_os_irq_init_param adxl355_gpio_irq_ip;
-#endif
-
-#ifdef IIO_LWIP_EXAMPLE
-extern uint8_t adin1110_mac_address[6];
-extern struct adin1110_init_param adin1110_ip;
-#endif
-
-#endif /* __COMMON_DATA_H__ */
+#endif /* __IIO_LWIP_EXAMPLE_H__ */
